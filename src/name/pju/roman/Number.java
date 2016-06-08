25867fcd1342b84d2275c4c1db44f14369a23d58
paul unger <unger.p@gmail.com>
2016-06-07 20:20:22 -0400

    Swap out Number body

--- src/name/pju/roman/Number.java
+++ src/name/pju/roman/Number.java
@@ -1,29 +1,39 @@
 package name.pju.roman;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
-/**
- * Created by paul on 6/4/2016.
- */
 public class Number {
+//    enum Numeral { i, v, x, l, c, d, m, V, X, L, C, D, M;
+//        public boolean isMultiple() {
+//            return this.ordinal() % 2 == 0;
+//        }
+//        public Numeral getSubtractor() {
+//            if (this == i) return null;
+//            return Numeral.values()[ordinal() - (2 - ordinal() % 2)];
+//        }
+//        public int getMaxinstances() { return isMultiple() ? 5 : 1; }
+//    }
 
-    private List<Numeral> value;
-    public boolean isValid() {
-        Numeral level = Numeral.highest();
-        Numeral last = null;
-        boolean exactlyOne = false;
-        for (Numeral n : value) {
-            if (n == last) {
-                if (!n.isAllowMultiple()) return false;
-                exactlyOne = false;
-            } else if (last != null) {
-                if (last.compareTo(n) > 0) {
-                    continue;
-                } else if (last.equals(n.getSubtractor())) {
+    private final List<Numeral> value;
+    private void normalize() {
+        Collections.sort(value);
 
-                } else return false;
-            }
-            last = n;
-        }
     }
+
+    public Number(String v) {
+        value = new ArrayList<>();
+        for (int i = 0; i < v.length(); i++) {
+            value.add(Numeral.valueOf(v.substring(i, i)));
+        }
+        normalize();
+    }
+    @Override
+    public String toString() {
+        return value.stream().map(n -> n.name()).collect(Collectors.joining());
+    }
+
+
 }
